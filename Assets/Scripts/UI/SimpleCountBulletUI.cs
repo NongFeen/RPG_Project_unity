4afceb757ad4ed1d0d8b68ca826a84039e2f50bd
--global <kakfeenza@gmail.com>
2026-04-17 05:17:01 +0700

    test : projectile

---
+++ Assets/Scripts/UI/SimpleCountBulletUI.cs
@@ -0,0 +1,23 @@
+using TMPro;
+using UnityEngine;
+
+public class SimpleCountBulletUI : MonoBehaviour
+{
+    public TextMeshProUGUI text;
+    public void Awake()
+    {
+        if(GameplayLogger.Instance == null)
+        {
+            Debug.LogError("GameplayLogger instance not found! Please ensure a GameplayLogger is present in the scene.");
+            this.enabled = false; 
+            text.gameObject.SetActive(false); 
+        }
+    }
+    void Update()
+    {
+        int count = NetworkObjectPool.Singleton != null
+                ? NetworkObjectPool.Singleton.ActiveObjectsCount
+                : 0;
+        text.text = $"Objects: {count}";
+    }
+}
